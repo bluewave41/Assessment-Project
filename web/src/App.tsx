0f6b18fb5012fba3d58fffaa57d26a3dd3ca937f
bluewave41 <megaman22342@live.ca>
2022-06-30 21:33:04 -0700

    Added final comment. 4 hours are up

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Box from '@mui/material/Box';
 
+//material UI font imports
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
